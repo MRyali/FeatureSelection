64e4cc8a62239f30dcf721e5a70922661aceb1e4
Mayur Ryali <mayurryali29@gmail.com>
2021-03-10 13:46:18 -0800

    Initial commit

--- accuracy.h
+++ accuracy.h
@@ -4,3 +4,14 @@
 //
 //  Created by Mayur Ryali on 3/10/21.
 //
+
+#ifndef accuracy_h
+#define accuracy_h
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#endif /* accuracy_h */
--- search.h
+++ search.h
@@ -4,3 +4,14 @@
 //
 //  Created by Mayur Ryali on 3/10/21.
 //
+
+#ifndef search_h
+#define search_h
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#endif /* search_h */
