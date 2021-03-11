346385e1b763ecbb6114b4ecb39091f51dad4425
Mayur Ryali <mayurryali29@gmail.com>
2021-03-11 15:58:06 -0800

    Add cross validation function

--- accuracy.cpp
+++ accuracy.cpp
@@ -20,6 +20,40 @@ double Euclidean_Distance(vector<double> vecA, <vector<double> vecB, vector<int>
     return sqrt(distance);
 }
 // function for Leave_ One_Out_Cross_Validation
+// Credit: Adopted from Dr. Keogh's Slides on cross validation
 double Leave_One_Out_Cross_Validation(vector<vector<double>> dataSet, vector<int> currFeatures) {
-    // write codde
+    // number of instances in data
+    double instances = (double)data.size();
+    // number of correct classifications
+    double correct = 0;
+
+    for(int i = 0; i < dataSet.size(); i++) {
+        vector<double> testData = data.at(i);
+        double nn_dist = INT_MAX; // distance to nearest neighbor
+        int nn_location = INT_MAX; // ocation of nearest neighbor
+        vector<double> nn_neighbor; // nearest neighbor
+
+        // find nearest neighbors
+        for (int k = 0; k < dataSet.size(); k++) {
+            // find distance between test and current data
+            if (k != i) {
+                distance = Euclidean_Distance(testData, dataSet.at(k), currFeatures);
+                if (distance < nn_dist) { // found new nearest neighbor
+                    nn_dist = distance;
+                    nn_location = k;
+                    nn_neighbor = data.at(k);
+                }
+            }
+        }
+
+        // check for correct classifications among nearest neighbors
+        if (nn_neighbor.at(0) == 1 && testData.at(0) == 1) {
+            correct += 1;
+        }
+        else if (nn_neighbor.at(0) == 2 && testData.at(0) == 2) {
+            correct += 1;
+        }
+    }
+    // calculate accuracy
+    return correct / instances;
 }
--- search.cpp
+++ search.cpp
@@ -4,3 +4,24 @@
 //
 //  Created by Mayur Ryali on 3/10/21.
 //
+
+#include "search.h"
+
+using namespace std;
+
+// function to check if feature is already in current set. Used in Forward and Backward functions
+bool featue_Is_In(vector<int> currSet, int num) {
+    for (int i = 0; i < currSet.size(); i++) {
+        if (currSet.at(i) == num) { // feature exists in current set
+            return true;
+        }
+    }
+    return false; //feature was not found in current set
+}
+// function for forward search
+vector<int> Forward_Search(vector<vector<double>>);
+// function for backward elimination
+void Backward_Elimination(vector<vector<double>>);
+// function to remove features to allow for us to progress through the iteration
+// used in Backward_Elimination
+vector<int> Remove_Feature(vector<int>, int);
