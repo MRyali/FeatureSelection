f91aae0df12fc8a2274e7693263249fa9422af96
Mayur Ryali <mayurryali29@gmail.com>
2021-03-10 21:47:49 -0800

    Add code to find euclidean distance

--- accuracy.cpp
+++ accuracy.cpp
@@ -4,3 +4,22 @@
 //
 //  Created by Mayur Ryali on 3/10/21.
 //
+
+#include "accuracy.h"
+
+using namespace std;
+
+// function to find euclidean distance between two elements using their features
+// Euclidean formula: https://www.geeksforgeeks.org/program-calculate-distance-two-points/
+double Euclidean_Distance(vector<double> vecA, <vector<double> vecB, vector<int> currFeatures) {
+    double distance = 0;
+
+    for(int i = 0; i < currSet.size(); i++) {
+        distance += (pow(vecA.at(currFeatures.at(i)) - vecB.at(currFeatures.at(i)), 2));
+    }
+    return sqrt(distance);
+}
+// function for Leave_ One_Out_Cross_Validation
+double Leave_One_Out_Cross_Validation(vector<vector<double>> dataSet, vector<int> currFeatures) {
+    // write codde
+}
